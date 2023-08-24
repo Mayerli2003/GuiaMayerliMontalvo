3f572d2f5134581bd463e5ad3d5c9a6b8310871e
Mayerli Azucena Montalvo <87994175+Mayerli2003@users.noreply.github.com>
2023-08-23 19:15:47 -0600

    Guia

---
+++ Ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1;
+
+/**
+ *
+ * @author mmaye
+ */
+import javax.swing.JOptionPane;
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,   " Bienvenido al mundo de Java ", "Resultados", JOptionPane.PLAIN_MESSAGE);
+      System.exit (0);
+    }
+}
